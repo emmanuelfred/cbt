8e440efacb429734c8c143a4f1f8dc7997023cea
emmanuelfred <emmanuelfredrick66@gmail.com>
2025-06-02 16:11:45 +0100

    Initial commit

--- src/Components/Header/Mobile/MobileHeader.js
+++ src/Components/Header/Mobile/MobileHeader.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './MobileHeader.css';
 import logo from '../../../Assets/mobile-logo.png'
 import { Link } from 'react-router-dom';
 
 const MobileHeader = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [showMainNav, setShowMainNav] = useState(false);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        setShowMainNav(window.scrollY > 50); // Change threshold as needed
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      // Cleanup on unmount
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <div className="mobile-header">
+    <div className={showMainNav ? "mobile-header shownav" : "mobile-header"}>
       <div className="header-bar">
         <Link to={'/'} className="logo"> <img src={logo} alt=""  /></Link>
         <button className="menu-toggle" onClick={toggleMenu}>
@@ -19,9 +33,9 @@ const MobileHeader = () => {
 
       <div className={`side-menu ${menuOpen ? 'open' : ''}`}>
         <ul>
-          <li><a href="#">Home</a></li>
+          <li><a href="/">Home</a></li>
           <li><a href="#">Courses</a></li>
-          <li><a href="#">About</a></li>
+          <li><a href="/about">About</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
       </div>
